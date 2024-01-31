164602cdeb906d0f567a3cf363ef8c1796a900d6
Calvin Liang <145249380+LiangCalvin@users.noreply.github.com>
2024-01-31 13:50:36 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+using CheckComDetail.Data;
+using Microsoft.EntityFrameworkCore;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDBContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+var app = builder.Build();
+
+//var dbContext = app.Services.GetRequiredService<ApplicationDBContext>();
+//dbContext.Database.Migrate();
+
+
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=LogIn}/{action=Index}/{id?}");
+
+app.Run();
